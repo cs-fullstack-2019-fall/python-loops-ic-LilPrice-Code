c19432d9abc53b0f47c32f46a77a25ffe23fa374
LilPrice-Code <littleprice002@gmail.com>
2019-08-21 14:47:52 -0500

    Completed

---
+++ index.py
@@ -0,0 +1,75 @@
+# print("Loop")
+
+# Exercise 1:
+# Create a program that takes user input. Print Loop. Nothing is done with the user input yet.
+#
+
+# while 0 == 0:
+#     user = input("Enter a word ")
+#     if user == "yet":
+#         break
+
+# Exercise 2:
+#
+# Create a program that takes user input and prints the user input.
+#
+# name = input("Enter Something ")
+# print(name)
+
+# Exercise 3:
+#
+# Create a program that takes user input in a while loop.
+# If they enter ‘q’ or 0 (your choice), quit.
+# Inside the loop print LOOP
+
+# while 0 == 0:
+#     user = input("Enter a word ")
+#     if user == "q" or user == 'x':
+#         break
+#     else:
+#         print("Loop")
+
+#
+# Exercise 4:
+#
+# Create a program that uses a Python range to loop from 50 to 10 and print each number in the console.
+#
+# num = 50
+# while num >= 10:
+#     print(num)
+#     num = num -1
+
+# Exercise 5:
+#
+# Create a program that will count down from 10 to 1 (print the number) then print LIFTOFF! in the console
+#
+
+# num = 10
+# while num > 0:
+#     print (num)
+#     num = num -1
+#
+# print("LIFTOFF!!")
+
+# Exercise 6:
+#
+# Create a program that prompts for the starting and ending number for a counting loop.
+# In the loop, count from the start number to end number entered by 5s.
+# Print each number in the console.
+
+num1 = int(input("Enter starting number "))
+num2 = int(input("Enter ending number "))
+
+while 0==0:
+    if num1 > num2:
+        print(num1)
+        num1 = num1 - 1
+    elif num1 < num2:
+        print(num1)
+        num1 = num1 + 1
+    elif num1 == num2:
+        print(num1)
+        break
+    else:
+        print("INVALID ANSWER")
+        break
